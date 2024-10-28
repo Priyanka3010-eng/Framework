02e321c425252c403236897322595f9e9613d1ed
MADDIMSETTY Sesha Satya Priyanka (Priyanka) <sesha.satya.priyanka.maddimsetty@hexagon.com>
2024-10-28 12:38:58 +0530

    Update

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET Framework!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
